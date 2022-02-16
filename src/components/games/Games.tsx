c578b9a60089a752087260c0b807607545f12623
Anton <antonskakyn999@gmail.com>
2022-02-16 21:38:36 +0300

    created data file and change in the search

--- src/components/games/Games.tsx
+++ src/components/games/Games.tsx
@@ -1,32 +1,14 @@
-import React, { FC, useState } from 'react'
-import {IArr} from "../../types/Arr"
-import Search from '../search/Search'
-import CardGame from './game-card/CardGame'
+import React, {FC} from 'react'
 import style from "./game-card/cardGame.module.scss"
+import {games} from "../../data/data"
+import Search from '../search/Search'
 
 const Games: FC = () => {
-    const [value, setValue] = useState<string>("")
-    const [games] = useState<IArr[]>([
-        {name: "PUBG", price: 30},
-        {name: "The witcher 3", price: 30},
-        {name: "Metro 2033", price: 30},
-    ])
-
-    const onChangeText = (e: React.ChangeEvent<HTMLInputElement>) => {
-      setValue(e.target.value)
-    }
-
-    const filtred = games.filter(game => {
-      return game.name.toLowerCase().includes(value.toLowerCase());
-    })
  
   return (
     <>
-      <Search value={value} onChangeText={onChangeText}/>
       <div className={style.cards}>
-          {filtred.map((game) => (
-            <CardGame key={game.name} name={game.name} price={game.price}/>
-          ))}
+          <Search games={games}/>
       </div>
     </>
   )
